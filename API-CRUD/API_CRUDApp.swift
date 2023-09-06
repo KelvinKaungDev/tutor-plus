0c23bc89b19b2755cb9a866bc65c4aef837364a7
Kelvin Gao <u6511753@au.edu>
2023-09-06 10:50:49 +0700

    Initial Commit

---
+++ API-CRUD/API_CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CRUDApp.swift
+//  API-CRUD
+//
+//  Created by Kelvin Gao  on 6/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct API_CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API-CRUD/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  API-CRUD
+//
+//  Created by Kelvin Gao  on 6/9/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
